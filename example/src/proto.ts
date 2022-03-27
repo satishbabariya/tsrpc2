0b2e3f716ec3e35355aa45f3c045633dc096c3ea
Satish Babariya <satish.babariya@gmail.com>
2022-03-27 23:36:10 +0530

    add server types generation

---
+++ example/src/proto.ts
@@ -0,0 +1,19 @@
+import { Service } from "tsprc2";
+
+interface EchoRequest {
+  message: string;
+}
+
+interface EchoResponse {
+  message: string;
+}
+
+interface EchoService extends Service {
+  echo(request: EchoRequest): EchoResponse;
+  get(): EchoResponse;
+}
+
+interface EchoService2 extends Service {
+  echo(request: EchoRequest): EchoResponse;
+  get(): EchoResponse;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import commander from "commander";
+import { InterfaceDeclaration, Project } from "ts-morph";
+
+// Service interface
+export interface Service {}
 
 const pkg = require("../package.json");
 
@@ -9,11 +13,100 @@ async function main() {
   program.version(`${pkg.version}`);
 
   program
-    .command("init")
-    .alias("i")
-    .description("Initialize a new project")
-    .action(() => {
-      console.log("Initializing a new project");
+    .command("generate")
+    .alias("g")
+    .description("Generate server and client code from proto files")
+    .requiredOption("-p, --proto-path <path>", "Path to proto files")
+    .requiredOption("-o, --output-path <path>", "Path to output files")
+    .option("--client", "Generate client code", true)
+    .option("--server", "Generate server code", true)
+    .action((options) => {
+      const { protoPath, outputPath, client, server } = options;
+      let project = new Project();
+      project.addSourceFileAtPath(protoPath);
+
+      if (server) {
+        let generatedFile = project.createSourceFile(
+          `${outputPath}/server.d.ts`,
+          "",
+          { overwrite: true }
+        );
+
+        let interfacesToExport: InterfaceDeclaration[] = [];
+
+        let sourceFiles = project.getSourceFiles();
+        sourceFiles.forEach((sourceFile) => {
+          let interfaces = sourceFile.getInterfaces();
+          interfaces.forEach((interfaceDeclaration) => {
+            let extendsTypes = interfaceDeclaration
+              .getExtends()
+              .map((extendsType) => extendsType.getText());
+
+            if (extendsTypes.includes("Service")) {
+              let unimplementedInterface = generatedFile.addInterface({
+                name: `I${interfaceDeclaration.getName()}`,
+                isExported: true,
+              });
+
+              let unimplementedMethods = interfaceDeclaration.getMethods();
+
+              unimplementedMethods.forEach((method) => {
+                let returnType = method.getReturnType().getText();
+                let responseType = returnType
+                  .replace("Promise<", "")
+                  .replace(">", "");
+
+                // find the response type from interfaces
+                let responseInterface = interfaces.find(
+                  (interfaceNode) => interfaceNode.getName() === responseType
+                );
+                // add the response type to the file
+                if (responseInterface) {
+                  if (!interfacesToExport.includes(responseInterface)) {
+                    interfacesToExport.push(responseInterface);
+                  }
+                }
+
+                let parameters = method
+                  .getParameters()
+                  .map((param) => param.getStructure());
+
+                parameters.forEach((param) => {
+                  let paramInterface = interfaces.find(
+                    (interfaceNode) => interfaceNode.getName() === param.type
+                  );
+
+                  if (paramInterface) {
+                    if (!interfacesToExport.includes(paramInterface)) {
+                      interfacesToExport.push(paramInterface);
+                    }
+                  }
+                });
+
+                unimplementedInterface.addMethod({
+                  name: method.getName(),
+                  parameters: parameters,
+                  returnType: `Promise<${responseType}>`,
+                });
+              });
+            }
+          });
+        });
+
+        // add the interfaces to the file
+        interfacesToExport.forEach((interfaceDeclaration) => {
+          generatedFile.addInterface({
+            ...interfaceDeclaration.getStructure(),
+            isExported: true,
+          });
+        });
+
+        generatedFile.formatText();
+        generatedFile.saveSync();
+      }
+
+      if (client) {
+      }
     });
 
   program.parse(process.argv);
